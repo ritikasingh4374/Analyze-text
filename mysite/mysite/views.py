6143387cb78a0c72210ae034f9eed425a943dbcd
ritikasingh4374 <154665560+ritikasingh4374@users.noreply.github.com>
2024-08-15 10:26:26 +0530

    using all fuctions together

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -9,43 +9,41 @@ def navigate(request):
     return render(request, 'navigate.html')
 
 def analyze(request):
-    djtext = (request.GET.get('text','default'))
-    remove = (request.GET.get('removepunc','off'))
-    removenew = (request.GET.get('removenew','off'))
-    allcap = (request.GET.get('allcap','off'))
-    removespace = (request.GET.get('removespace','off'))
+    djtext = (request.POST.get('text','default'))
+    remove = (request.POST.get('removepunc','off'))
+    removenew = (request.POST.get('removenew','off'))
+    allcap = (request.POST.get('allcap','off'))
+    removespace = (request.POST.get('removespace','off'))
     print(djtext)
     print(remove)
     print(allcap)
     if remove == "on":
-        punctuations = '''@#$%%^^&&**(){}[]'''
+        punctuations = '''@#$%%^^&&**(){}[]"""'''
         analysed = ""
         for char in djtext:
             if char not in punctuations:
                 analysed += char
-            
 
         params = {'purpose': 'Removed punctuations', 'analysed_text' : analysed}
-        return render(request, 'analyze.html', params)
+        djtext = analysed
     
     if allcap == "on":
         analysed = ""
         for char in djtext:
             analysed += char.upper()
-            
-
+        
         params = {'purpose': 'capitalised', 'analysed_text' : analysed}
-        return render(request, 'analyze.html', params)
+        djtext = analysed
     
     if removenew == "on":
         analysed = ""
         for char in djtext:
-            if char!= "\n":
+            if char!= "\n" and char != '\r':
                 analysed += char
             
-
+        
         params = {'purpose': 'removenew', 'analysed_text' : analysed}
-        return render(request, 'analyze.html', params)
+        djtext = analysed
     
     if removespace == "on":
         analysed = ""
@@ -53,9 +51,8 @@ def analyze(request):
             if char!= ' ':
                 analysed += char
             
-
+       
         params = {'purpose': 'removespace', 'analysed_text' : analysed}
-        return render(request, 'analyze.html', params)
-    
-    else :
-        return HttpResponse("error")
+        djtext = analysed
+
+    return render(request, 'analyze.html', params)
